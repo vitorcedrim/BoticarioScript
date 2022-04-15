138911d633fb25ffca4d1151d627127e973992f5
vitorcedrim <vk.cedrim@gmail.com>
2022-04-15 15:24:38 -0300

    Implementação do OAuth2 e outros
    Código completamente reconstruído.
    OAuth2 implementado, conexão direta com o Drive API.
    Implementado sistema para deletar automaticamente os arquivos CSV baixados, aprimorando a segurança.
    Implementada lógica responsável por detectar se deve ser realizado o upload, exclusão ou atualização dos arquivos no Drive.
    Criado arquivo appConfig.py que funciona como singleton, contendo todas as informações necessárias para rodar o código (Links dos arquivos CSV e ID da pasta de destino no Drive).
    Renovação automática dos Tokens OAuth2.
    
    ATENÇÃO!
    
    Na pasta do script deve ser adicionado o arquivo de credenciais do usuário, gerado no Google Cloud API.
    O primeiro uso do script deve ser realizado em máquina com suporte a interface gráfica e navegador web para que o login no Google possa ser realizado. O login é necessário apenas na conta que receberá os arquivos enviados pelo script.

--- app.py
+++ app.py
@@ -1,6 +1,121 @@
+from __future__ import print_function
+from fileinput import filename
+
+import os
 import requests as re
+import re as r
 
-url = f'https://docs.google.com/spreadsheets/d/e/2PACX-1vS-r3TsvVzpM0LScbenDYHyGnquWSJEJ863fxzyYQdpuHkDfEcSQHmgocDgbq5Att41oX8oDLTQQxwy/pub?gid=1879265991&single=true&output=csv'
-data = re.get(url)
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from apiclient.http import MediaFileUpload
+from apiclient import errors
 
-open('2022.csv', 'wb').write(data.content)
+from appConfig import urls
+from appConfig import destinyFolderID
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+
+def mainProcess():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+
+    # Realiza o download das planilhas.
+
+        for url in urls:
+            data = re.get(url)
+            headerData = data.headers['content-disposition']
+            titleRaw = r.findall("filename=(.+)", headerData)[0]
+            fileName = r.findall(r'"(.*?)"', titleRaw)[0]
+            open(fileName, 'wb').write(data.content)
+            existingFilesIDArray = []
+            existingFilesNameArray = []
+
+            # Call the Drive v3 API
+
+            service = build('drive', 'v3', credentials=creds)
+
+            page_token = None
+            while True:
+                response = service.files().list(q=f"mimeType='text/csv' and name='{fileName}' and trashed=false and '{destinyFolderID}' in parents",
+                                                    spaces='drive',
+                                                    fields='nextPageToken, files(id, name)',
+                                                    pageToken=page_token).execute()
+                for file in response.get('files', []):
+                    existingFilesIDArray.append(file.get('id'))
+                    existingFilesNameArray.append(file.get('name'))
+
+                print(existingFilesNameArray)
+                print(existingFilesIDArray)
+
+                if len(existingFilesIDArray) > 1:
+                    for existingFile in existingFilesIDArray:
+                        service.files().delete(fileId=existingFile).execute()
+                        print('Deletei um arquivo!')
+
+                    # Upload do arquivo da vez
+
+                    file_metadata = {'name': fileName, 'parents': [destinyFolderID]}
+                    media = MediaFileUpload(f'./{fileName}', mimetype='text/csv', resumable=True)
+                    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+                    print('Fiz um upload!')
+
+                if len(existingFilesIDArray) == 1:
+
+                    # File's new content.
+                    media_body = MediaFileUpload(
+                        fileName, mimetype='text/csv', resumable=True)
+
+                    # Send the request to the API.
+                    service.files().update(
+                        fileId=f'{existingFilesIDArray[0]}',
+                        media_body=media_body).execute()
+                    print('Fiz um update!')
+
+                if len(existingFilesIDArray) == 0:
+                    # Upload do arquivo da vez
+
+                    file_metadata = {'name': fileName, 'parents': [destinyFolderID]}
+                    media = MediaFileUpload(f'./{fileName}', mimetype='text/csv', resumable=True)
+                    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+                    print('Fiz um upload!') 
+
+                page_token = response.get('nextPageToken', None)
+                if page_token is None:
+                    break
+
+            # Deleta os arquivos CSV.
+
+            os.remove(fileName)
+
+
+    except HttpError as error:
+        # TODO(developer) - Handle errors from drive API.
+        print(f'An error occurred: {error}')
+
+
+mainProcess()
